69647e532feb2fe107fb9196c05f15dab5789d85
Dizhan Xue <xuedizhan17@mails.ucas.ac.cn>
2022-12-01 22:10:20 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -4,10 +4,9 @@ import math
 
 
 class Loss(nn.Module):
-    def __init__(self, batch_size, temperature_f, temperature_c, temperature_s):
+    def __init__(self, batch_size, temperature_c, temperature_s):
         super(Loss, self).__init__()
         self.batch_size = batch_size
-        self.temperature_f = temperature_f
         self.temperature_c = temperature_c
         self.temperature_s = temperature_s
 
@@ -23,24 +22,6 @@ class Loss(nn.Module):
             mask[N//2 + i, i] = 0
         mask = mask.bool()
         return mask
-
-    def forward_feature(self, h_i, h_j):
-        N = 2 * self.batch_size
-        h = torch.cat((h_i, h_j), dim=0)
-
-        sim = torch.matmul(h, h.T) / self.temperature_f
-        sim_i_j = torch.diag(sim, self.batch_size)
-        sim_j_i = torch.diag(sim, -self.batch_size)
-
-        positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(N, 1)
-        mask = self.mask_correlated_samples(N)
-        negative_samples = sim[mask].reshape(N, -1)
-
-        labels = torch.zeros(N).to(positive_samples.device).long()
-        logits = torch.cat((positive_samples, negative_samples), dim=1)
-        loss = self.criterion(logits, labels)
-        loss /= N
-        return loss
 
     def forward_label(self, q_i, q_j):
         q_i = q_i.t()
